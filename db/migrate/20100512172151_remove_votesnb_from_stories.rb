26b5bcf514bcb3a315bd3bf1d9bad2f5c47cba5f
stephane <stephane@stephane-desktop.(none)>
2010-09-07 21:40:39 +0200

    tensoon

--- db/migrate/20100512172151_remove_votesnb_from_stories.rb
+++
@@ -1,9 +0,0 @@
-class RemoveVotesnbFromStories < ActiveRecord::Migration
-  def self.up
-    remove_column :stories, :votesnb
-  end
-
-  def self.down
-    add_column :stories, :votesnb, :string
-  end
-end
