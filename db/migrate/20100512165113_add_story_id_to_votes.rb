883c9c24312cd6c8879c0733d57af4ab312fc8d4
stephane <stephane@stephane-desktop.(none)>
2010-09-07 21:34:27 +0200

    tensoon

--- db/migrate/20100512165113_add_story_id_to_votes.rb
+++
@@ -1,9 +0,0 @@
-class AddStoryIdToVotes < ActiveRecord::Migration
-  def self.up
-    add_column :votes, :story_id, :integer
-  end
-
-  def self.down
-    remove_column :votes, :story_id
-  end
-end
