710e4b64b77dff8e9b7324c0edce882619265ce8
stephane <stephane@stephane-desktop.(none)>
2010-09-12 11:33:02 +0200

    tensoon

--- app/models/story.rb
+++ app/models/story.rb
@@ -20,7 +20,7 @@ attr_accessor :rules
 validates_presence_of :titre, :snip , :url , :category
 validates_length_of :titre, :maximum=>100 , :if => :titre?
 validates_format_of :url, :with => /http:\/\// , :if => :url?
-validates_length_of :snip, :minimum=>200 , :if => :snip?
+validates_length_of :snip, :minimum=>150 , :if => :snip?
 validates_length_of :snip, :maximum=>600 , :if => :snip?
 validate :lessthan1hr
 validates_acceptance_of :rules
